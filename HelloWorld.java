9b1d199d38b2426499ea4d6a11b74301ca9dcdee
hibajeha10 <114784198+hibajeha10@users.noreply.github.com>
2022-10-07 01:02:34 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world 2");
-        System.out.println("hello world 4");
+        /*
+        some
+        codes
+        for
+        loginpage
+        */
 
     }
 }
