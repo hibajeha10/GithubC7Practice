2b8184ff2a355643491c6bb95b898d69a86726cf
hiba jeha <hibajeha@hibas-MacBook-Air.local>
2022-10-07 00:46:46 -0400

    practice

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world 2");
+        System.out.println("hello world 4");
 
     }
 }
